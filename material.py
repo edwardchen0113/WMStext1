22ac97fa9e0c59d527ea70f310e6ea1fc56c625b
unknown <edwardchen0113@gmail.com>
2019-11-19 08:01:33 +0800

    增加檢查檔案是否存在

--- material.py
+++ material.py
@@ -1,11 +1,20 @@
-materials = []
-with open('materials.csv','r') as f:
-	for line in f:
-		if '異動類型,儲位,料號,數量' in line:
-			continue
-		move, location, material, quantity = line.strip().split(',')
-		materials.append([move,location, material, quantity])
+import os #載入os功能(operating system)
 
+#讀取檔案
+materials = []
+if os.path.isfile('materials.csv'): #用OS的功能旗下的檢查路徑(path)及檢查檔案(isfile)來確認檔案是否存在
+	print('檔案確認無誤') #此行可不寫
+#讀取資料(用來將先前資料讀出後再將新資料一起寫入，避免新資料直接蓋掉舊資料造成舊資料不見)	
+	with open('materials.csv','r') as f:
+		for line in f:
+			if '異動類型,儲位,料號,數量' in line:
+				continue
+			move, location, material, quantity = line.strip().split(',')
+			materials.append([move,location, material, quantity])
+else:
+	print('檔案不存在，將重新產生新的檔案')
+
+#使用者輸入
 while True:
 	move = input('請輸入異動類型：')
 	if move == 'q':
@@ -24,10 +33,12 @@ while True:
 			quantity = int(quantity)
 			materials.append([move,location, material, quantity])
 
+# 印出目前所有資料內容
 for m in materials:
 	print('異動類型：', m[0], '儲位：', m[1], '料號：', m[2], '數量：', m[3])
 
-with open('materials.csv', 'w') as f: #encoding = 'utf-8'世界語言通用編碼，防止寫成CSV檔時出現亂碼。
+#寫入CSV檔
+with open('materials.csv', 'w') as f: 
 	f.write('異動類型,儲位,料號,數量\n',)
 	for m in materials:
 		f.write(m[0]+ ','+ m[1]+ ','+ m[2]+ ','+ str(m[3])+ '\n')
